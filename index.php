ebcb72f4e532e2c03d517a702dbdcd3eba73d50e
Gustavo Êrades Vilarinho Silva <108373134+Gustavo-erades@users.noreply.github.com>
2023-10-03 13:54:05 -0300

    organizando

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <!-- fonte awesome -->
     <script src="https://kit.fontawesome.com/574d869779.js" crossorigin="anonymous"></script>
     <!-- Style customizado -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./estilo/style.css">
     <!-- Style bootsrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Converter Decimal</title>
